ac14f95be0e85eca6e76615dac78a5074eeccfc0
Ahmedx59 <Ahmedx59@gmail.com>
2024-09-06 19:09:48 +0300

    add company models

--- company/models.py
+++ company/models.py
@@ -1,2 +1,18 @@
 from django.db import models
 
+class Branch(models.Model):
+    name = models.CharField(max_length=50)
+    address = models.CharField(max_length=100)
+    phone = models.CharField(max_length=12, null = True , blank = True)
+    email = models.EmailField(max_length=50, null = True , blank = True)
+
+    def __str__(self):
+        return self.name
+
+class Department(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.TextField(max_length=200)
+    branch = models.ForeignKey('Branch', related_name="departmentsBranch", on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
