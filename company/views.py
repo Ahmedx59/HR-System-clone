ef229a8cdbbe80239e79506ad4bd9c276d4dabcb
Ahmedx59 <Ahmedx59@gmail.com>
2024-09-06 19:20:18 +0300

    add company view , base , branches html

--- company/views.py
+++ company/views.py
@@ -1,2 +1,16 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Branch, Department
 
+
+
+
+def branches(request):
+    branches = Branch.objects.all()
+    return render(request, 'company/branches.html', {'branches':branches})
+
+
+def branchDetails(request, branch_id):
+    branch = Branch.objects.get(id=branch_id)
+    departments = branch.departmentsBranch.all()
+    return render(request, 'company/branchDetails.html', {'branch':branch, 'departments':departments})
